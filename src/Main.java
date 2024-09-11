c335b0c52a29fb0a9dae5cbc47d255e018dd432d
lischloff <lisa.schloffer@gmail.com>
2024-09-11 08:09:41 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//Author: Schloffer Lisa
+//LBS Eibiswald | 2aAPC | 11.09.2024
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Quadrat: " + (400 * 400));
+    }
+}
